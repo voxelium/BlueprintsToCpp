685c96a73e758a999281d26f06835e62665f3d54
VOXELIUM <52653488+voxelium@users.noreply.github.com>
2022-08-28 15:49:16 +0300

    Delete Plugins/Developer/RiderLink directory

--- Plugins/Developer/RiderLink/Intermediate/Build/Mac/x86_64/UnrealEditor/Development/RD/hashing.cpp.d
+++
@@ -1,3 +0,0 @@
-/Volumes/DataFiles/my_CLOUDS/YandexDisc/WIP/BlueprintsToCpp/Plugins/Developer/RiderLink/Intermediate/Build/Mac/x86_64/UnrealEditor/Development/RD/hashing.cpp.o: \
-  /Volumes/DataFiles/my_CLOUDS/YandexDisc/WIP/BlueprintsToCpp/Plugins/Developer/RiderLink/Source/RD/src/rd_framework_cpp/src/main/util/hashing.cpp \
-  /Volumes/DataFiles/my_CLOUDS/YandexDisc/WIP/BlueprintsToCpp/Plugins/Developer/RiderLink/Intermediate/Build/Mac/x86_64/UnrealEditor/Development/RD/Definitions.h
--- Plugins/Developer/RiderLink/Intermediate/Build/Mac/x86_64/UnrealEditor/Development/RD/thirdparty.cpp.d
+++
@@ -1,3 +0,0 @@
-/Volumes/DataFiles/my_CLOUDS/YandexDisc/WIP/BlueprintsToCpp/Plugins/Developer/RiderLink/Intermediate/Build/Mac/x86_64/UnrealEditor/Development/RD/thirdparty.cpp.o: \
-  /Volumes/DataFiles/my_CLOUDS/YandexDisc/WIP/BlueprintsToCpp/Plugins/Developer/RiderLink/Source/RD/thirdparty/thirdparty.cpp \
-  /Volumes/DataFiles/my_CLOUDS/YandexDisc/WIP/BlueprintsToCpp/Plugins/Developer/RiderLink/Intermediate/Build/Mac/x86_64/UnrealEditor/Development/RD/Definitions.h
